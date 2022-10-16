2f920a130377c4b3cc15ce04fc1f33223597535f
Wysciguwka <bartek14411@gmail.com>
2022-10-17 00:15:42 +0200

    Fix temp dir

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from pisklak import create_app
 
 
 @pytest.fixture(scope="session")
-def app():
+def app(tmpdir_factory):
     app = create_app()
+    app.config['UPLOAD_FOLDER'] = tmpdir_factory.mktemp('uploads')
     return app
